31010ca5d0a55e928b5d7333c7c7ff4d57c2217e
Solargi <79482908+Solargi@users.noreply.github.com>
2022-10-20 18:30:08 +0200

    18 formatting topics according to fiware iot agents (#20)
    * refactored street_lights_client code, implemented fiware protocol for topics, (TODO: sensor status update and error handling)
    fixes #18 and #19

--- traffic/street_lights_client.py
+++ traffic/street_lights_client.py
@@ -1,104 +1,145 @@
-# MQTT Client demo
+# MQTT Street Lights Client
 # Continuously monitor two different MQTT topics for data,
 # check if the received data matches two predefined 'commands'
- 
 import paho.mqtt.client as mqtt
+import json
 from grovepi import *
 from time import sleep
-from configparser import ConfigParser 
-
+from configparser import ConfigParser
+# read values from config ini
 file = 'config.ini'
 config = ConfigParser()
 config.read(file)
-#config connection
-broker_address=str(config['connection']['broker_address'])
+
+
+# topic constructor
+def topic_constructor(protocol, service_api_key, sensor_id):
+    return ("/" + protocol + "/" + service_api_key + "/" + sensor_id)
+
+
+def attr_topic(topic):
+    return (topic + "/attrs")
+
+
+def cmd_topic(topic):
+    return (topic + "/cmd")
+
+
+def ack_topic(cmd_topic):
+    return (cmd_topic + "exe")
+
+
+# config connection
+broker_address = str(config['connection']['broker_address'])
 port = int(config['connection']['port'])
 username = str(config['connection']['username'])
 password = str(config['connection']['password'])
-#######street1 config
-#street led 1 topic
-vcln_topic=str(config['Street 1']['vcln_topic'])
-sl1_topic = str(config['Street 1']['lt'])
-vcln_pin = int(config['Street 1']['vcln_pin'])
-#led pins
-l1_pin = int(config['Street 1']['l1_pin'])
-l2_pin = int(config['Street 1']['l2_pin'])
-l3_pin = int(config['Street 1']['l3_pin'])
-l4_pin = int(config['Street 1']['l4_pin'])
-#######street2 config
-#street led 1 topic
-os_topic = str(config['Street 2']['os_topic'])
-sl2_topic= str(config['Street 2']['lt'])
-os_pin = int(config['Street 2']['os_pin'])
-l5_pin = int(config['Street 2']['l1_pin'])
-l6_pin = int(config['Street 2']['l2_pin'])
-l7_pin = int(config['Street 2']['l3_pin'])
-l8_pin = int(config['Street 2']['l4_pin'])
+    
+# topic base config
+protocol = str(config['connection']['protocol'])
+service_api_key = str(config['street_lights']['lights_service_api_key'])
 
-##flag to see when connection active
-connected = False
-##keep car count
-car_counter = 0
+# streetlight 1 def, considered 1 port for 2 led to be changed if 4 led fit in one port
+# Use digital pins for led from 2 to 8
+sl1 = json.loads(config.get("street_lights", "sl1"))
+sl1_id = str(config['street_lights']['sl1_id'])
+sl1_topic = topic_constructor(protocol, service_api_key, sl1_id)
+
+# streetlight 2 def considered 1 port for 2 led to be changed if 4 led fit in one port
+# Use digital pins for led from 2 to 8
+sl2 = json.loads(config.get("street_lights", "sl2"))
+sl2_id = str(config['street_lights']['sl2_id'])
+sl2_topic = topic_constructor(protocol, service_api_key, sl2_id)
+
+# returns true if operation successfull
+def set_lights(setter, pins):
+    status = 0
+    if (setter == "on"):
+        for pin in pins:
+            status += digitalWrite(pin, 1)
+        if(status == len(pins)):
+            return True
+
+    elif (setter == "off"):
+        for pin in pins:
+            status += digitalWrite(pin, 0)
+        if status == len(pins):
+            return True
+    else:
+        print("setter invalid")
+        return False
+
+# send light ack
+def light_cmd_ack(client, msg, payload, status):
+    # prep ack object
+    payload['light']['switch'] = status
+    # send ack with json
+    topic = ack_topic(str(msg.topic))
+    print("sending ack to topic:" + str(topic))
+    client.publish(ack_topic(str(msg.topic)), json.dumps(payload))
+
+
+def update_light_status_attribute(client, msg, state):
+    topic = attr_topic(str(msg.topic)[:-4])
+    print("sending status update to topic: " + str(topic))
+    client.publish(attr_topic(
+        str(msg.topic)[:-4]), json.dumps({"powerState": state}))
+
+# execute light comand
+def light_cmd_exe(msg, client, light_pin):
+    payload = json.loads(str(msg.payload))
+    try:
+        if payload['light']['switch'] == "on":
+            print("turning on lights ...")
+            if (set_lights("on", light_pin)):
+                light_cmd_ack(client, msg, payload, "ok")
+                update_light_status_attribute(client, msg, "on")
+
+            else:
+                light_cmd_ack(client, msg, payload,
+                              "error: failed to turn on lights")
+        elif payload['light']['switch'] == "off":
+            print("turning off lights ...")
+            if (set_lights("off", light_pin)):
+                light_cmd_ack(client, msg, payload, "ok")
+                update_light_status_attribute(client, msg, "off")
+            else:
+                light_cmd_ack(client, msg, payload,
+                              "error: failed to turn off lights")
+    except Exception as err:
+        print("error, expected command/argument :" +
+              str(err) + ", received: " + str(msg.payload))
+
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-                print("Client successfully connected to broker with result code "+str(rc))
-                # Subscribing in on_connect() - if we lose the connection and
-                # reconnect then subscriptions will be renewed.
-                #subscribe to light topics to listen for lights on
-                client.subscribe(sl1_topic) 
-                client.subscribe(sl2_topic) 
-                connected = True
-        else:
-                print("Failed connecting to Broker Check credentials and broker address")
-                connected = False 
+    if rc == 0:
+        print("Client successfully connected to broker with result code "+str(rc))
+        # Subscribing in on_connect() - if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        # subscribe to light topics to listen for lights on
+        client.subscribe(cmd_topic(sl1_topic))
+        client.subscribe(cmd_topic(sl2_topic))
+    else:
+        print("Failed connecting to Broker Check credentials and broker address")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-
-    if msg.payload == "turn on light" and msg.topic == sl1_topic:
-        print("turning on lights on street 1 ...")
-        l1 = digitalWrite(l1_pin,1)
-        l2 = digitalWrite(l2_pin,1)
-        l3 = digitalWrite(l3_pin,1)
-        l4 = digitalWrite(l4_pin,1)
-        if l1==l2 and l2==l3 and l3==l4 and l4==1 :
-            client.publish(sl1_topic, "light on")
-        ### turn off light after 5 sec
-            sleep(5)
-            l1 = digitalWrite(l1_pin,0)
-            l2 = digitalWrite(l2_pin,0)
-            l3 = digitalWrite(l3_pin,0)
-            l4 = digitalWrite(l4_pin,0)
-
-    if msg.payload == "turn on light" and msg.topic == sl2_topic:
-        print("turning on lights on street 1 ...")
-        l5 = digitalWrite(l5_pin,1)
-        l6 = digitalWrite(l6_pin,1)
-        l7 = digitalWrite(l7_pin,1)
-        l8 = digitalWrite(l8_pin,1)
-        if l5==l6 and l6==l7 and l7==l8 and l8==1 :
-            client.publish(sl2_topic, "light on")
-            ### turn off light after 5 sec
-            sleep(5)
-            l5 = digitalWrite(l5_pin,0)
-            l6 = digitalWrite(l6_pin,0)
-            l7 = digitalWrite(l7_pin,0)
-            l8 = digitalWrite(l8_pin,0)
-
+    print(msg.topic+" " + str(msg.payload))
+    if msg.topic == (cmd_topic(sl1_topic)):
+        light_cmd_exe(msg, client, sl1)
+    elif msg.topic == (cmd_topic(sl2_topic)):
+        light_cmd_exe(msg, client, sl2)
 # Create an MQTT client and attach our routines to it.
 client = mqtt.Client()
-#client.username_pw_set(username,password) #uncomment when using local broker
+if username is not None and password is not None:
+    client.username_pw_set(username,password)
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(broker_address, port, 60)
- 
+
 # Process network traffic and dispatch callbacks. This will also handle
 # reconnecting. Check the documentation at
 # https://github.com/eclipse/paho.mqtt.python
 # for information on how to use other loop*() functions
 client.loop_forever()
-
-
