cd560f2c327a10e98b5b5d15535d7b81872fadf0
Solargi <solarg@usi.ch>
2022-10-20 13:30:43 +0200

    added some debug code and corrected some bugs

--- traffic/TrafficCounter.py
+++ traffic/TrafficCounter.py
@@ -16,7 +16,7 @@ from configparser import ConfigParser
 #IMPORTANT: When assigning the port to the sensor assign the next port
 #           from where the sensor has been connected
 #           e.g.: if sensor connectet to d4 assign the sensor to port d5
-#           otherweise the sensor won't work. also leave port d5 free
+#           otherweise the sensor won't work. also leave port d5 and d3 free
 #           to ensure the sensor works correctly
 
 # Connect the Grove Infrared Distance Interrupt Sensor to digital port D5
@@ -54,12 +54,12 @@ protocol = "json"
 service_api_key = "test_key"#str(config['Street 2']['service_api_key'])
 
 ##ir Sensor data and functions
-##Connect the ir sensor at port d5 (see initial note)
-ir_pin = 6
+##Connect the ir sensor at port d6 NOT d7 (see initial note)
+ir_pin = 7
 grovepi.pinMode(ir_pin,"INPUT")
 ir_id = "ir"
 ir_topic = topic_constructor(protocol, service_api_key, ir_id)
-ir_car_counter = 0
+# ir_car_counter = 0
 
 
 
@@ -70,7 +70,7 @@ os_pin = 8
 grovepi.pinMode(os_pin,"INPUT")
 os_id = "os"
 os_topic = topic_constructor(protocol, service_api_key, os_id)
-os_car_counter = 0
+# os_car_counter = 0
 
 ##update sensor counter
 def update_counter(sensor_pin, counter):
@@ -78,7 +78,7 @@ def update_counter(sensor_pin, counter):
         try:
             # Sensor returns LOW and onboard LED lights up when the
             # received infrared light intensity exceeds the calibrated level
-            if grovepi.digitalRead(ir_pin) == 0:
+            if grovepi.digitalRead(ir_pin) == 1:
               #increase counter if car found
               counter += 1
             else:
@@ -89,14 +89,14 @@ def update_counter(sensor_pin, counter):
         try:
              ## obstacle sensor no obstacle:   
             if grovepi.digitalRead(os_pin) == 1:
-                print ("no car detected")
+                print ("os_sendor: no car detected")
             ###obstacle:
             else:
                 counter += 1
         except IOError:
             print ("IO Error")
     else:
-        print("Passed invalid sensor pin, valid pins: " + str(os_pin) + ", " + str(ir_pin) + "\n counter update failed returnin passed counter:" )
+        print("Passed invalid sensor pin: " + str(sensor_pin) + ", valid pins: " + str(os_pin) + ", " + str(ir_pin) + "\n counter update failed returnin passed counter:" )
     return counter
 
 
@@ -111,18 +111,22 @@ def pub_counter(counter,  sensor_base_topic,  authentication, broker_address, po
     #convert to json
     counter_obj = json.dumps(counter_obj)
     publish.single(pub_topic, payload=counter_obj, hostname=broker_address, port=port_number, auth=authentication)
+    print("published: " +str(counter) + " on topic: " + pub_topic)
 
 
 def monitor_traffic(ir_pin, os_pin):
+    ir_car_counter = 0
+    os_car_counter = 0
     while True:
-        try:
-            ir_car_counter = update_counter(ir_pin)
-            os_car_counter = update_counter(os_pin)
+        # try:
+            ir_car_counter = update_counter(ir_pin, ir_car_counter)
+            os_car_counter = update_counter(os_pin, os_car_counter)
             #publish counter
             pub_counter(ir_car_counter, ir_topic, None, broker_address, port) #authentication not used for now change none with auth param when using private broker
             pub_counter(os_car_counter, os_topic, None, broker_address, port) #authentication not used for now change none with auth param when using private broker
-        except:
-            print ("Something went wrong")
+            sleep(2) #used to limit traffic to public mqtt server
+        # except:
+        #     print ("Something went wrong")
 
 
 ##start monitoring
