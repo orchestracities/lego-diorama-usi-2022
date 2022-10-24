b061b78f2561b0730f223bc8029ed1029514e558
casarf <51021963+casarf@users.noreply.github.com>
2022-10-24 09:02:30 +0200

    Milestone 0.2 (#21)
    * fix #6 
    * fix #7

---
+++ diorama/src/environment/cayenne_environment_seed_martel_002/cayenne_environment_seed_martel_002.ino
@@ -0,0 +1,217 @@
+// code for seed-martel-002
+
+// Multichannel Gas Sensor on I2C
+// Air Quality Sensor on A0
+// Sound Sensor on A1
+// UV_sensor on A2
+
+#include <LoRaWan.h>
+
+#include <CayenneLPP.h>
+CayenneLPP lpp(100);
+
+#include <Wire.h>
+
+#include <string.h>
+#include <stdlib.h>
+
+#include <Multichannel_Gas_GMXXX.h>
+#ifdef SOFTWAREWIRE
+    #include <SoftwareWire.h>
+    SoftwareWire myWire(3, 2);
+    GAS_GMXXX<SoftwareWire> gas;
+#else
+    #include <Wire.h>
+    GAS_GMXXX<TwoWire> gas;
+#endif
+
+#define DEBUG 1 // 1 to show debugging print in serial monitor, 0 to hide them
+
+//air quality sensor
+#include "Air_Quality_Sensor.h"
+
+AirQualitySensor sensor(A0);
+
+//sound sensor
+const int pinSound = A1;
+//int thresholdValue = 200;
+
+//UVSensor
+const int pinUV_Sensor = A2;
+
+static uint8_t recv_cmd[8] = {};
+
+//unsigned char data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA,};
+char buffer[256];
+
+//MEASURE VALUES
+
+//multichannelGasSensor
+uint32_t NO2 = 0, C2H5OH = 0, VOC = 0, CO = 0;
+
+//AirQualitySensor
+uint32_t quality = 0;
+
+//AirQualitySensor status
+//FORCE_SIGNAL 3;
+//HIGH_POLLUTION 2;
+//LOW_POLLUTION 1;
+//FRESH_AIR 0;
+
+//soundSensor
+int soundValue = 0;
+
+//UVSensor
+long UVvalue = 0;
+
+
+void setup(void) {
+    SerialUSB.begin(115200);
+    // while (!SerialUSB)
+    //     ;
+
+    //Lorawan Chip Config
+    lorawanConfig();
+
+    //multichannel_gas_sensor on I2C
+    multGasSensorSetup();
+
+    //air quality sensor
+    airQualitySetup();
+}
+
+void loop(void) {
+
+  lpp.reset(); //reset lpp to send new message
+
+  // MultGasSensor
+  multGasSensorLoop();
+  
+  lpp.addDigitalInput(1,NO2);
+  lpp.addDigitalInput(2,C2H5OH);
+  lpp.addDigitalInput(3,VOC);
+  lpp.addDigitalInput(4,CO);
+
+  // AirQualitySensor
+  airQualityLoop();
+
+  lpp.addDigitalInput(5, quality);
+
+  // SoundSensor
+  soundLoop();
+
+  lpp.addDigitalInput(6,soundValue);
+
+  // UVSensor
+  UVSensorLoop();
+
+  lpp.addDigitalInput(7, UVvalue);
+
+  bool msg1 = lora.transferPacketWithConfirmed(lpp.getBuffer(),lpp.getSize(),5);
+  delay(4000);
+}
+
+// settings for the connection with the gateway
+void lorawanConfig() {
+    lora.init();
+
+    memset(buffer, 0, 256);
+    lora.getVersion(buffer, 256, 1);
+    SerialUSB.print(buffer);
+
+    memset(buffer, 0, 256);
+    lora.getId(buffer, 256, 1);
+    SerialUSB.print(buffer);
+
+    // void setId(char *DevAddr, char *DevEUI, char *AppEUI);
+    lora.setId(NULL, "70B3D57ED00561F1", "8CF95720000569A6");
+    // setKey(char *NwkSKey, char *AppSKey, char *AppKey);
+    lora.setKey(NULL, NULL, "9C14C735E02914422112AC32D7C0EC27");
+
+    lora.setDeciveMode(LWOTAA);
+    lora.setDataRate(DR0, EU868);
+
+    lora.setChannel(0, 868.1);
+    lora.setChannel(1, 868.3);
+    lora.setChannel(2, 868.5);
+
+    lora.setReceiceWindowFirst(0, 868.1);
+    lora.setReceiceWindowSecond(869.5, DR3);
+
+    lora.setDutyCycle(false);
+    lora.setJoinDutyCycle(false);
+
+    lora.setPower(14);
+
+    while (!lora.setOTAAJoin(JOIN))
+        ;
+}
+
+
+// MULTICHANNEL GAS SENSOR
+void multGasSensorSetup() {
+    gas.begin(Wire, 0x08); // use the hardware I2C
+}
+
+void multGasSensorLoop() {
+    NO2 = gas.measure_NO2();
+    C2H5OH = gas.measure_C2H5OH();
+    VOC = gas.measure_VOC();
+    CO = gas.measure_CO();
+}
+
+// AIR QUALITY SENSOR
+void airQualitySetup() {
+    Serial.println("Waiting sensor to init...");
+    delay(20000);
+
+    if (sensor.init()) {
+        Serial.println("Sensor ready.");
+    } else {
+        Serial.println("Sensor ERROR!");
+    }
+}
+
+void airQualityLoop() {
+    int slope = sensor.slope();
+
+    if(DEBUG){
+      Serial.print("Sensor value: ");
+      Serial.println(sensor.getValue());
+    }  
+    
+    if (slope == AirQualitySensor::FORCE_SIGNAL) {
+        quality = 3;
+    } else if (slope == AirQualitySensor::HIGH_POLLUTION) {
+        quality = 2;
+    } else if (slope == AirQualitySensor::LOW_POLLUTION) {
+        quality = 1;
+    } else if (slope == AirQualitySensor::FRESH_AIR) {
+        quality = 0;
+    }
+
+}
+
+// SOUND SENSOR
+int soundLoop()
+{
+    soundValue = analogRead(pinSound);   //read the sensorValue on Analog 0
+    if (DEBUG) Serial.println(soundValue);
+}
+
+//UV SENSOR
+void UVSensorLoop()
+{
+    int sensorValue;
+    long  sum=0;
+    for(int i=0;i<1024;i++)// accumulate readings for 1024 times
+    {
+        sensorValue=analogRead(pinUV_Sensor);
+        //if(sensorValue) Serial.println(sensorValue);
+        sum=sensorValue+sum;
+        delay(2);
+    }
+    long meanVal = sum/1024;  // get mean value
+
+    UVvalue = (meanVal*1000/4.3-83)/21;
+}
---
+++ diorama/src/waste/main_waste_seed_martel_001/main_waste_seed_martel_001.ino
@@ -0,0 +1,233 @@
+//code for seed-martel-001
+
+//Temp sensor on D5
+//Fire sensor on D2
+//Dist Sensor on D3
+//Accelerometer on I2C
+
+#include <LoRaWan.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Wire.h>
+#include "MMA7660.h"
+#include "DHT.h"
+#include "Ultrasonic.h"
+
+#define DEBUG 1  //1 to show debugging print in serial monitor, 0 to hide them
+
+//accelerometer def
+MMA7660 accelerometer;
+
+//Temp&Hum def
+#define DHTTYPE DHT22 // DHT 22  (AM2302)
+#define DHTPIN 5      // what pin we're connected to（DHT10 and DHT20 don't need define it）
+DHT dht(DHTPIN, DHTTYPE);
+
+//digital pins def
+#define FLAME_SENSOR 2  //connect SENSOR to digital pin1
+
+Ultrasonic ultrasonic(3); //Distance Sensor
+
+unsigned char data[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA,};
+char buffer[256];
+
+
+void setup(void) {
+  SerialUSB.begin(115200);
+  // while (!SerialUSB)
+  //   ;
+
+  //Lorawan Chip Config
+  lorawanConfig();
+
+  //Accelerometer
+  accelerometerSetup();
+
+  //Temperature & Humidity
+  tempHumSetup();
+
+  //FLAME SENSOR
+  pinMode(FLAME_SENSOR, INPUT);
+}
+
+void loop(void) {
+
+  //Accelerometer Message
+  char accelerometerMSG[100];
+  strcpy(accelerometerMSG,accelerometerLoop());
+  bool msg1 = lora.transferPacketWithConfirmed(accelerometerMSG, 4);
+  delay(4000);
+
+  //Flame Sensor Message
+  bool flameBool = isFlameDetected();
+  bool msg2 = flameBool ? lora.transferPacketWithConfirmed("Fire detected",4) :
+                          lora.transferPacketWithConfirmed("Fire not detected",4);
+  delay(4000);
+
+  //Temperature & Humidity Message
+  char TempMsg[50];
+  strcpy(TempMsg,tempHumLoop());
+  bool msg3 = lora.transferPacketWithConfirmed(TempMsg,4);
+  delay(4000);
+
+  //Distance Msg
+  char DistMsg[50];
+  strcpy(DistMsg,distanceLoop());
+  bool msg4 = lora.transferPacketWithConfirmed(DistMsg,4);
+  delay(4000);
+
+}
+
+
+//settings for the connection with the gateway
+void lorawanConfig() {
+  lora.init();
+
+  memset(buffer, 0, 256);
+  lora.getVersion(buffer, 256, 1);
+  SerialUSB.print(buffer);
+
+  memset(buffer, 0, 256);
+  lora.getId(buffer, 256, 1);
+  SerialUSB.print(buffer);
+
+  // void setId(char *DevAddr, char *DevEUI, char *AppEUI);
+  lora.setId(NULL, "70B3D57ED00561EC", "8CF957200005727C");
+  // setKey(char *NwkSKey, char *AppSKey, char *AppKey);
+  lora.setKey(NULL, NULL, "51F58CAC3F5735E3D1F88DD3EADBE9C6");
+
+  lora.setDeciveMode(LWOTAA);
+  lora.setDataRate(DR0, EU868);
+
+  lora.setChannel(0, 868.1);
+  lora.setChannel(1, 868.3);
+  lora.setChannel(2, 868.5);
+
+  lora.setReceiceWindowFirst(0, 868.1);
+  lora.setReceiceWindowSecond(869.5, DR3);
+
+  lora.setDutyCycle(false);
+  lora.setJoinDutyCycle(false);
+
+  lora.setPower(14);
+
+  while (!lora.setOTAAJoin(JOIN))
+    ;
+}
+
+//FLAME SENSOR
+
+bool isFlameDetected() {
+  if (DEBUG) Serial.println(!digitalRead(FLAME_SENSOR));
+  return !digitalRead(FLAME_SENSOR);
+}
+
+//accelerometer
+void accelerometerSetup() {
+  accelerometer.init();
+  Serial.begin(9600);
+}
+
+char * accelerometerLoop() {
+  int8_t x;
+  int8_t y;
+  int8_t z;
+  float ax, ay, az;
+
+  char * accelerometerMessage = (char *) malloc(100);
+  char sensorValue[6]= "";
+
+
+  accelerometer.getXYZ(&x, &y, &z);
+
+  sprintf(sensorValue, "%i",x);
+  strcpy(accelerometerMessage, "x: ");
+  strcat(accelerometerMessage,sensorValue);
+
+
+  sprintf(sensorValue, "%i",y);
+  strcat(accelerometerMessage, ", y: ");
+  strcat(accelerometerMessage,sensorValue);
+
+  sprintf(sensorValue, "%i",z);
+  strcat(accelerometerMessage, ", z: ");
+  strcat(accelerometerMessage,sensorValue);
+
+
+  if (DEBUG) {
+    Serial.print("x = "); Serial.println(x); Serial.print("y = ");
+    Serial.println(y); Serial.print("z = "); Serial.println(z);
+  }
+
+  accelerometer.getAcceleration(&ax, &ay, &az);
+
+  sprintf(sensorValue, "%.2f",ax);
+  strcat(accelerometerMessage, ", ax: ");
+  strcat(accelerometerMessage,sensorValue);
+
+
+  sprintf(sensorValue, "%.2f",ay);
+  strcat(accelerometerMessage, ", ay: ");
+  strcat(accelerometerMessage,sensorValue);
+
+  sprintf(sensorValue, "%.2f",az);
+  strcat(accelerometerMessage, ", az: ");
+  strcat(accelerometerMessage,sensorValue);
+
+
+  if (DEBUG) {
+    Serial.println("accleration of X/Y/Z: "); Serial.print(ax); Serial.println(" g");
+    Serial.print(ay); Serial.println(" g"); Serial.print(az); Serial.println(" g");
+    Serial.println("*************");
+  }
+
+  return accelerometerMessage;
+}
+
+// Temp & Hum
+void tempHumSetup() {
+    Wire.begin();
+    dht.begin();
+}
+
+char * tempHumLoop() {
+    
+    float temp_hum_val[2] = {0};
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+
+    char * tempHumMessage = (char *) malloc(50);
+    char buf[6]= "";
+
+    if (!dht.readTempAndHumidity(temp_hum_val)) {
+        strcpy(tempHumMessage, "Hum: ");
+        sprintf(buf, "%.2f",temp_hum_val[0]);
+        strcat(tempHumMessage, buf);
+        strcat(tempHumMessage, ", ");
+        strcat(tempHumMessage, "Temp: ");
+        sprintf(buf, "%.2f",temp_hum_val[1]);
+        strcat(tempHumMessage, buf);;
+    } else {
+        strcpy(tempHumMessage, "Failed to get temp/hum.");
+    }
+    Serial.println(tempHumMessage);
+    return tempHumMessage;
+}
+
+char * distanceLoop() {
+    int RangeInCentimeters;
+
+    char * distanceMessage = (char *) malloc(50);
+    char buf2[6]= "";
+
+    strcpy(distanceMessage,"Distance: ");
+
+    RangeInCentimeters = (int)ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+    sprintf(buf2, "%i",RangeInCentimeters);
+    strcat(distanceMessage,buf2);
+    strcat(distanceMessage," cm");
+
+    Serial.println(distanceMessage);
+
+    return distanceMessage;
+}
